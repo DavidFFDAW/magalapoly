e451b4f6a5c37212d2e583f53776f120e6528557
DavidFFDAW <davidferflo2@gmail.com>
2023-02-24 00:41:24 +0100

    feat: login form

--- pages/auth/login/index.jsx
+++ pages/auth/login/index.jsx
@@ -1,23 +1,49 @@
-import PageWrapper from "@/components/PageWrapper";
+import React from 'react';
+import PageWrapper from '@/components/PageWrapper';
 
-export default function login() {
+export default function Login() {
+    const [formData, setFormData] = React.useState({ email: '', password: '' });
+
+    const handleSubmit = ev => {
+        ev.preventDefault();
+        console.log(formData);
+    };
+
     return (
-        <PageWrapper title={'Login'}>
-            <div className='login-page'>
-                <div className='login-form'>
-                <h1>Login</h1>
-                <form>
-                    <div className='form-group'>
-                    <label htmlFor='email'>Email</label>
-                    <input type='email' name='email' id='email' />
+        <>
+            <PageWrapper title={'Login'}>
+                <div className="login-page">
+                    <div className="flex center al-center column gap login-form">
+                        <h1>Login</h1>
+                        <form action="POST" onSubmit={handleSubmit}>
+                            <div className="form-group">
+                                <label htmlFor="email" className="text">
+                                    Email
+                                </label>
+                                <input
+                                    className="custom form-item"
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    onChange={ev => setFormData({ ...formData, email: ev.target.value })}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password" className="text">
+                                    Password
+                                </label>
+                                <input
+                                    className="custom form-item"
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    onChange={ev => setFormData({ ...formData, password: ev.target.value })}
+                                />
+                            </div>
+                        </form>
                     </div>
-                    <div className='form-group'>
-                    <label htmlFor='password'>Password</label>
-                    <input type='password' name='password' id='password' />
-                    </div>
-                </form>
                 </div>
-            </div>
-        </PageWrapper>
-    )
+            </PageWrapper>
+        </>
+    );
 }
