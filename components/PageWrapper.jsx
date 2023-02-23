bba47c7028f3e531e6053feb87ea3c800ae0be9a
DavidFFDAW <davidfernandez@madeinmedia.es>
2023-02-23 08:56:07 +0100

    chore: PageWrapper auth prop support for auth

--- components/PageWrapper.jsx
+++ components/PageWrapper.jsx
@@ -1,14 +1,23 @@
 import Head from "next/head";
+import AuthRedirector from "./AuthRedirector";
 
-export default function PageWrapper({ title, replace = false, children }) {
+export default function PageWrapper({ title, replace = false,  auth, children }) {
     const titleText = replace ? title : `MagalaPoly | ${title}`;
+
+    const Auth0 = Boolean(auth) ? (
+        <AuthRedirector>
+            <div className="page-wrapper">{children}</div>
+        </AuthRedirector>
+    ) : (
+        <div className="page-wrapper">{children}</div>
+    );
 
     return (
         <>
             <Head>
                 <title>{titleText}</title>
             </Head>
-            <div className="page-wrapper">{children}</div>
+            { Auth0 }
         </>
     );
 }
