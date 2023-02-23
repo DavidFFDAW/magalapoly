f839e21f1111d298fc512cb940ee2cc184d1e52a
DavidFFDAW <davidfernandez@madeinmedia.es>
2023-02-23 08:56:28 +0100

    feat: login page and redirect logic on auth page

--- pages/auth/login/index.jsx
+++ pages/auth/login/index.jsx
@@ -1,4 +1,23 @@
-export default function login({ cookies }) {
-    console.log(cookies);
-    return <div>login</div>;
+import PageWrapper from "@/components/PageWrapper";
+
+export default function login() {
+    return (
+        <PageWrapper title={'Login'}>
+            <div className='login-page'>
+                <div className='login-form'>
+                <h1>Login</h1>
+                <form>
+                    <div className='form-group'>
+                    <label htmlFor='email'>Email</label>
+                    <input type='email' name='email' id='email' />
+                    </div>
+                    <div className='form-group'>
+                    <label htmlFor='password'>Password</label>
+                    <input type='password' name='password' id='password' />
+                    </div>
+                </form>
+                </div>
+            </div>
+        </PageWrapper>
+    )
 }
