56364b0d3254e75df1fe98c1a5fe6a940b0c30da
DavidFFDAW <davidferflo2@gmail.com>
2023-02-24 20:00:44 +0100

    feat: add login page with no functionality yet

--- pages/auth/login/index.jsx
+++ pages/auth/login/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import PageWrapper from '@/components/PageWrapper';
+import Input from '@/components/Forms/Input';
+import { FormButton } from '@/components/Forms/Button';
+import CustomImage from '@/components/Image';
+import NativeLink from '@/components/NativeLink';
 
 export default function Login() {
     const [formData, setFormData] = React.useState({ email: '', password: '' });
@@ -13,34 +17,46 @@ export default function Login() {
         <>
             <PageWrapper title={'Login'}>
                 <div className="login-page">
-                    <div className="flex center al-center column gap login-form">
-                        <h1>Login</h1>
-                        <form action="POST" onSubmit={handleSubmit}>
-                            <div className="form-group">
-                                <label htmlFor="email" className="text">
-                                    Email
-                                </label>
-                                <input
-                                    className="custom form-item"
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    onChange={ev => setFormData({ ...formData, email: ev.target.value })}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="password" className="text">
-                                    Password
-                                </label>
-                                <input
-                                    className="custom form-item"
-                                    type="password"
-                                    name="password"
-                                    id="password"
-                                    onChange={ev => setFormData({ ...formData, password: ev.target.value })}
-                                />
+                    <div className="login-form">
+                        <CustomImage
+                            src={'/icons/magalapoly.png'}
+                            alt={'logo'}
+                            width={200}
+                            height={200}
+                            className="login-image"
+                        />
+                        {/* <h1>Login</h1> */}
+                        <form action="POST" onSubmit={handleSubmit} className="flex center al-center column gap">
+                            <Input
+                                label={'Email:'}
+                                name={'email'}
+                                type={'email'}
+                                onChange={ev => setFormData({ ...formData, email: ev.target.value })}
+                                placeholder={'example@gmail.com'}
+                                autocomplete={'email'}
+                            />
+
+                            <Input
+                                label={'Password:'}
+                                name={'password'}
+                                type={'password'}
+                                onChange={ev => setFormData({ ...formData, password: ev.target.value })}
+                                placeholder={'********'}
+                                autocomplete={'current-password'}
+                            />
+
+                            <div className="w-100 flex between al-center gap">
+                                <NativeLink href={'/'} className="w-100 btn btn-form">
+                                    Cancelar
+                                </NativeLink>
+                                <FormButton text={'Login'} className="w-100 btn-red" />
                             </div>
                         </form>
+                        <div className="flex center down">
+                            <span onClick={ev => console.log(ev)} className="red pointer">
+                                He olvidado mi contraseña
+                            </span>
+                        </div>
                     </div>
                 </div>
             </PageWrapper>
