a62132bd8688cc27b1759982823ebe0e65cdd184
DavidFFDAW <davidferflo2@gmail.com>
2023-09-28 13:27:20 +0200

    delete: main branch delete all content

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  mode: 'production',
-  disableDevLogs: true,
-})
-
-module.exports = withPWA()
--- pages/admin/dashboard.jsx
+++
@@ -1,9 +0,0 @@
-import PageWrapper from '@/components/PageWrapper';
-
-export default function dashboard() {
-    return (
-        <PageWrapper title={'Dashboard'}>
-            <h1>Dashboard</h1>
-        </PageWrapper>
-    );
-}
