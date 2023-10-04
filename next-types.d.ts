8c53a40b598f5c5b2a9e757d0216dcfc3c37f078
DavidFFDAW <davidferflo2@gmail.com>
2023-10-04 11:20:01 +0200

    fix: solved augmentation of next-auth

--- next-types.d.ts
+++ next-types.d.ts
@@ -1,5 +1,37 @@
+import { User } from './src/models/user.model';
+import 'next-auth'
 import { PushSubscription } from 'web-push';
 
 declare global {
     var subscriptions: PushSubscription[];
+}
+
+
+/* Not sure if this is needed. */
+// declare module 'next-auth' {
+//   export interface Session {
+//     uid: string
+//   }
+// }
+
+import { DefaultSession, DefaultUser } from "next-auth"
+import { JWT, DefaultJWT } from "next-auth/jwt"
+
+declare module "next-auth" {
+    interface Session {
+        user: {
+            id: string,
+            role: string,
+        } & DefaultSession
+    }
+
+    interface User extends DefaultUser {
+        role: string,
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT extends DefaultJWT {
+        role: string,
+    }
 }
